fd6c3d6d4b0254098c2e223dc5ad2a27061f5f45
Mohammad Faiz Khan <s.mohdfaiz.khan@gmail.com>
2025-08-24 16:39:37 +0530

    Fix newline escape character in console.log statements
    Replaced incorrect '\n' with '/n' in multiple console.log statements to correct the output formatting in Assignment-2.js.

--- assignments/Assignment-2.js
+++ assignments/Assignment-2.js
@@ -21,7 +21,7 @@ const capitalizeWords = (sentence) =>
     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 
-    console.log("\n2. capitalizeWords:");
+    console.log("/n2. capitalizeWords:");
 console.log(capitalizeWords("hello world from javascript"));
 
 // Q3) Write a function called printSquares that takes an array of numbers as input and uses the forEach method to print the square of each number.
@@ -30,7 +30,7 @@ function printSquares(arr) {
   arr.forEach(num => console.log(num * num));
 }
 
-console.log("\n3. printSquares:");
+console.log("/n3. printSquares:");
 printSquares([1, 2, 3, 4]); 
 
 // Q4) Write a function called getDoubledValues that takes an array of numbers as input and uses the map method to return a new array with each number doubled.
@@ -39,7 +39,7 @@ function getDoubledValues(arr) {
   return arr.map(num => num * 2);
 }
 
-console.log("\n4. getDoubledValues:");
+console.log("/n4. getDoubledValues:");
 console.log(getDoubledValues([1, 2, 3])); 
 
 // Q5) Write a function called getEvenNumbers that takes an array of numbers as input and uses the filter method to return a new array with only the even numbers.
@@ -48,14 +48,14 @@ function getEvenNumbers(arr) {
   return arr.filter(num => num % 2 === 0);
 }
 
-console.log("\n5. getEvenNumbers:");
+console.log("/n5. getEvenNumbers:");
 console.log(getEvenNumbers([1, 2, 3, 4, 5, 6])); 
 
 // Q6) Write an arrow function called getMax that takes an array of numbers as a parameter and returns the maximum number in the array using the reduce method.
 
 const getMax = (arr) => arr.reduce((max, num) => num > max ? num : max, arr[0]);
 
-console.log("\n6. getMax:");
+console.log("/n6. getMax:");
 console.log(getMax([3, 7, 2, 9, 5]));
 
 
